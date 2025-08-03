ef8b04db9b7bff8a377b384db34acc5d184fcff1
Benno van den Bogaard <bennovandenbogaard@gmail.com>
2025-08-03 23:25:53 +0200

    Add main

---
+++ ha_dashboard.koplugin/main.lua
@@ -0,0 +1,62 @@
+local DataStorage = require("datastorage")
+local LuaSettings = require("luasettings")
+local WidgetContainer = require("ui/widget/container/widgetcontainer")
+local logger = require("logger")
+local _ = require("gettext")
+
+local HADashboard = WidgetContainer:extend {
+    name = "ha_dashboard",
+    settings_file = DataStorage:getSettingsDir() .. "/ha_dashboard.lua",
+}
+
+function HADashboard:addToMainMenu(menu_items)
+    menu_items.ha_dashboard = {
+        text = _("Home Assistant Dashboard"),
+        sorting_hint = "main",
+        callback = function()
+            self:open()
+        end,
+    }
+end
+
+function HADashboard:init()
+    logger:setLevel(logger.levels.dbg) --  Set logger level to debug for detailed output
+
+    self.loadSettings(self)
+    self.ui.menu:registerToMainMenu(self)
+
+    logger.dbg("HADashboard: initialized")
+end
+
+function HADashboard:cleanup()
+    logger.dbg("HADashboard: cleaned up")
+end
+
+function HADashboard:loadSettings()
+    logger.dbg("HADashboard: Looking for settings file at " .. self.settings_file)
+
+    self.settings = LuaSettings:open(self.settings_file)
+    if next(self.settings.data) == nil then
+        logger.dbg("HADashboard: No settings file found, using default settings")
+        self.updated = true
+        self.settings.data = require("settings")
+    else
+        logger.dbg("HADashboard: Settings file found, using existing settings")
+    end
+
+    logger.dbg("HADashboard: Using Home Assistant base url " .. self.settings.data.base_url)
+end
+
+function HADashboard:onFlushSettings()
+    if self.updated then
+        self.settings:flush()
+        self.updated = nil
+        logger.dbg("HADashboard: Settings flushed")
+    end
+end
+
+function HADashboard:open()
+    logger.dbg("HADashboard: opened")
+end
+
+return HADashboard
---
+++ ha_dashboard.koplugin/settings.lua
@@ -0,0 +1,5 @@
+return {
+    base_url = "",
+    token = "",
+    entities = {}
+}
