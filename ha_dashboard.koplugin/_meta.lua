e0b0eea94347720129d411f862ba22360c7bc4d4
Benno van den Bogaard <bennovandenbogaard@gmail.com>
2025-08-05 20:38:12 +0200

    Fix typo

--- ha_dashboard.koplugin/_meta.lua
+++ ha_dashboard.koplugin/_meta.lua
@@ -1,6 +1,6 @@
 local _ = require("gettext")
 return {
     name = "ha_dashboard",
-    fullname = _("Home Assitant Dashboard"),
+    fullname = _("Home Assistant Dashboard"),
     description = _([[A plugin for KOReader that allows you to control devices connected to Home Assistant.]]),
 }
